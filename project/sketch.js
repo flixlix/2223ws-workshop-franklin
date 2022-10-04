6071bade716546c9c8778e0e1f0175e6956f8bf2
Tim von Asen <timvonasen@users.noreply.github.com>
2022-10-04 15:40:22 +0200

    Update sketch.js

--- project/sketch.js
+++ project/sketch.js
@@ -3,3 +3,29 @@ let launchData;
 function preload(){
     launchData = loadTable('data/launches_by_country.csv', 'csv', 'header');
 }
+
+/* 
+Hochschule für Gestaltung - Schwäbisch Gmünd
+Grundlagen im medialen Raum - Projektarbeit
+Anton Pelezki 
+Carina Senger 
+Luca Mário Ziegler Félix  
+Tim Niedermeier 
+
+
+                             ___-------___
+                         _-~~             ~~-_
+                      _-~                    /~-_
+   /^\__/^\         /~  \                   /    \
+ /|  O|| O|        /      \_______________/        \
+| |___||__|      /       /                \          \
+|          \    /      /                    \          \
+|   (_______) /______/                        \_________ \
+|         / /         \                      /            \
+ \         \^\\         \                  /               \     /
+   \         ||           \______________/      _-_       //\__//
+     \       ||------_-~~-_ ------------- \ --/~   ~\    || __/
+       ~-----||====/~     |==================|       |/~~~~~
+        (_(__/  ./     /                    \_\      \.
+               (_(___/                         \_____)_)
+*/
